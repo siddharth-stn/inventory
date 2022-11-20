fc073e6860bca0cb8ffa2124b9a139236fc6376b
Siddharth Pande <sidarjunstn.sp@gmail.com>
2022-11-20 10:27:00 +0530

    Category model created and exported

--- models/category.js
+++ models/category.js
@@ -1,1 +1,13 @@
 const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+  name: { type: String, required: true },
+});
+
+CategorySchema.virtual("url").get(function () {
+  return `/article/category/${this._id}`;
+});
+
+module.exports = mongoose.model("Category", CategorySchema);
