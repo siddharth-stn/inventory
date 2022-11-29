00877307a33e84fbed985a4c08f8ae2c8a40582b
siddharth-stn <sidarjunstn.sp@gmail.com>
2022-11-29 19:34:15 +0530

    adding routes

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ db.on("error", () => {
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+const articleRouter = require("./routes/article");
 
 var app = express();
 
@@ -37,6 +38,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/article", articleRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/article.js
+++ routes/article.js
@@ -1,2 +1,29 @@
 const express = require("express");
 const router = express.Router();
+
+/* GET index page */
+router.get("/", (req, res, next) => {
+  res.send("Index Page");
+});
+
+/* GET item list page */
+router.get("/item", (req, res, next) => {
+  res.send("Item List Page");
+});
+
+/* GET iteminstance list page */
+router.get("/iteminstance", (req, res, next) => {
+  res.send("Item Instance List Page");
+});
+
+/* GET Brand list page */
+router.get("/brand", (req, res, next) => {
+  res.send("Brand List Page");
+});
+
+/* GET category list page */
+router.get("/category", (req, res, next) => {
+  res.send("Category List Page");
+});
+
+module.exports = router;
