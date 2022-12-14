83b4a6c7ef17b2f14d2ec0633b7fc75dca167c8f
Siddharth Pande <sidarjunstn.sp@gmail.com>
2022-12-14 21:22:28 +0530

    initialized item routes, have to work on brand routes

--- routes/article.js
+++ routes/article.js
@@ -7,15 +7,54 @@ const category_controller = require("../controllers/categoryController");
 const item_instance_controller = require("../controllers/item_instanceController");
 const item_controller = require("../controllers/itemController");
 
-/* GET index page */
+/// ITEM ROUTES ///
+
+/* GET article home page */
 router.get("/", (req, res, next) => {
   res.send("Index Page");
 });
 
-/* GET item list page */
+// GET request for creating an item. Note this must come before the routes that displat Item using id.
+router.get("/item/create", (req, res, next) => {
+  res.send("Item Creation GET Page");
+});
+
+// POST request for creating an item.
+router.post("/item/create", (req, res, next) => {
+  res.send("Item Creation POST Page");
+});
+
+// GET request for deleting an item.
+router.get("/item/:id/delete", (req, res, next) => {
+  res.send("Item deletion GET Page");
+});
+
+// POST request for deleting an item.
+router.post("/item/:id/delete", (req, res, next) => {
+  res.send("Item deletion POST Page");
+});
+
+// GET request for updating an item.
+router.get("/item/:id/update", (req, res, next) => {
+  res.send("Item updation GET Page");
+});
+
+// POST request for updating an item.
+router.post("/item/:id/update", (req, res, next) => {
+  res.send("Item updation POST Page");
+});
+
+// GET request for one item.
+router.get("/item/:id", (req, res, next) => {
+  res.send("GET request to Show one item using its id");
+});
+
+/* GET request for list of all items */
 router.get("/item", (req, res, next) => {
   res.send("Item List Page");
 });
+
+//! Have to start working here with brand routes
 
 /* GET iteminstance list page */
 router.get("/iteminstance", (req, res, next) => {
